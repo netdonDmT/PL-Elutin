17cfbdfefff43449c4a64fdc3d18becd62c0651b
netdonDmT <elutindanil11@gmail.com>
2025-10-23 20:03:43 +0300

    Update pr5_3.py

--- pr5_3.py
+++ pr5_3.py
@@ -1,7 +1,14 @@
 text = input()
 
+<<<<<<< Updated upstream
 print("Количество замен: ",sp.count("."))
 
+=======
+
+
+print("Количество замен: ",text.count("."))
+
+>>>>>>> Stashed changes
 text = text.replace(".", "")
 print(text)
 
