cc601c8699dfd1016afe8b2fe15ee0aa5c6c1738
netdonDmT <elutindanil11@gmail.com>
2025-11-06 11:11:30 +0300

    Update pr8_8_2.py

--- pr8_8_2.py
+++ pr8_8_2.py
@@ -1,26 +1,20 @@
 def f(matrix):
-
-    n = len(matrix)
     
-    transposed = [[0] * n for i in range(n)]
+    transposed = [[0] * len(matrix) for i in range((len(matrix)))]
     
-    for i in range(n):
-        for j in range(n):
+    for i in range(len(matrix)):
+        for j in range(len(matrix)):
             transposed[j][i] = matrix[i][j]
     
     return transposed
 
-# Пример использования
 matrix = [
     [1, 2, 3],
     [4, 5, 6],
     [7, 8, 9]
 ]
 
-
-
 result = f(matrix)
-
 
 for row in result:
     print(row)
